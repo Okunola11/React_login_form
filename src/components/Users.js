e0fa01f4aa51bc9c52ee454a8915cbc54925d6f5
Okunola Abdulwasiu <abdayoade@gmail.com>
2024-01-02 20:10:28 +0100

    Tweaked Users.js to allow mounting and unmounting

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import useAxiosPrivate from "../hooks/useAxiosPrivate";
 import { useNavigate, useLocation } from "react-router-dom";
 
@@ -7,31 +7,37 @@ const Users = () => {
   const axiosPrivate = useAxiosPrivate();
   const navigate = useNavigate();
   const location = useLocation();
+  const effectRan = useRef(false);
 
   useEffect(() => {
-    let isMounted = true;
-    const controller = new AbortController();
+    if (!effectRan.current) {
+      //let isMounted = true;
+      //const controller = new AbortController();
 
-    const getUsers = async () => {
-      try {
-        const response = await axiosPrivate.get("/users", {
-          signal: controller.signal,
-        });
-        const userName = response.data.map((user) => user.username);
-        console.log(response.data);
-        isMounted && setUsers(userName);
-      } catch (err) {
-        console.error(err);
-        // To sends the user to login page once the refreshToken expires and return them back to the page they were
-        // This runs with the navigate route already PUT in login page.
-        navigate("/login", { state: { from: location }, replace: true });
-      }
-    };
-    getUsers();
+      const getUsers = async () => {
+        try {
+          const response = await axiosPrivate.get(
+            "/users" /* {
+            signal: controller.signal,
+          } */
+          );
+          const userName = response.data.map((user) => user.username);
+          console.log(response.data);
+          /* isMounted && */ setUsers(userName);
+        } catch (err) {
+          console.error(err);
+          // To sends the user to login page once the refreshToken expires and return them back to the page they were
+          // This runs with the navigate route already PUT in login page.
+          navigate("/login", { state: { from: location }, replace: true });
+        }
+      };
+      getUsers();
+    }
 
     return () => {
-      isMounted = false;
-      isMounted && controller.abort();
+      effectRan.current = true;
+      //isMounted = false;
+      ///* isMounted && */ controller.abort();
     };
   }, []);
 
